9ad77319933fd6a5707818dcd6801faf9f1aa6a5
Ibrahem <ibrahem.al-radaei@outlook.com>
2026-07-08 04:30:12 +0300

    .

---
+++ usage/all.sh
@@ -0,0 +1,68 @@
+
+python3 c9/pwb.py core1/arwiki/params_report.py
+python3 c9/pwb.py arwiki/redcat.py
+python3 c9/pwb.py arwiki/rm.py
+python3 c9/pwb.py arwiki/sug.py
+python3 c9/pwb.py arwiki/temptitle.py -cat:تصنيف:قوالب_تصفح_باسم_خاطئ
+python3 c9/pwb.py asa/ass4_gen.py main3 -cat:تصنيف:مقالات_بحاجة_لمراجعة_الصنف
+python3 c9/pwb.py asa/ass4_gen.py main3 -ns:0,10,14 -newpages:50 llang:enfr
+python3 c9/pwb.py asa/ass4_gen.py main3 -ns:0,10,14 -recentchanges:200 llang:enfr
+python3 c9/pwb.py asa/ass4_gen.py main3 -ns:14 -newpages:50 newway llang:enfr
+python3 c9/pwb.py asa/bio.py -qudda:io
+python3 c9/pwb.py asa/bio.py sql
+python3 c9/pwb.py asa/cate.py addpro allxer
+python3 c9/pwb.py asa/import.py project:all
+python3 c9/pwb.py asa/infobox.py
+python3 c9/pwb.py asa/ssql.py main3 sqladdclass limit:3000 llang:enfr
+python3 c9/pwb.py asa1/as_by_project.py all
+python3 c9/pwb.py asa1/assu.py
+python3 c9/pwb.py asapages/bot.py
+python3 c9/pwb.py asapages/bot2.py
+python3 c9/pwb.py auths/del.py
+python3 c9/pwb.py c18/cat.py -depth:4 -range:6 -newpages2:100
+python3 c9/pwb.py c18/cat2.py -ns:14 -usercontribs:Mr.Ibrahembot ssubcat -limit:150
+python3 c9/pwb.py c18/catar.py workarcat -links:ويكيبيديا:مشروع_ويكي_الصيانة/التصنيفات_غير_المصنفة
+python3 c9/pwb.py c18/ennew_24.py -depth:5
+python3 c9/pwb.py c18/newcat.py -ns:14 -newpages:20
+python3 c9/pwb.py c18/newcat.py -ns:14 -newpages:30 usesql
+python3 c9/pwb.py c18/newcat.py -ns:14 -subcats:تصنيف:تصنيفات_غير_مستخدمة
+python3 c9/pwb.py c18/newcat.py -ns:14 -usernewpages:Mr.Ibrahembot -limit:100
+python3 c9/pwb.py c18/nnew.py 2040 -sqllimit:200
+python3 c9/pwb.py c18/search_cats.py
+python3 c9/pwb.py catsn/tempcat.py -newpages2:50
+python3 c9/pwb.py catsn/tempcat.py -ns:0 -recentchanges:50
+python3 c9/pwb.py catsn/tempcat.py -ns:10 gen_ar_template -newpages:100
+python3 c9/pwb.py cite/reff.py -newpages2:50
+python3 c9/pwb.py cite/reff.py -ns:0 -recentchanges:50
+python3 c9/pwb.py cite/reff.py makenewarchive -ns:0 -recentchanges:1000
+python3 c9/pwb.py cy/jsub.py -cat:تصنيف:سجل_فوز_دراج_من_ويكي_بيانات/قوالب
+python3 c9/pwb.py des/fam.py
+python3 c9/pwb.py ill/f.py -ns:10 main3
+python3 c9/pwb.py ill/f.py main3
+python3 c9/pwb.py ill/ill2.py -newpages:100 ill2
+python3 c9/pwb.py ill/illwd2.py -ns:0,10 main3
+python3 c9/pwb.py ill/new.py
+python3 c9/pwb.py mkn/a.py all_o_keys
+python3 c9/pwb.py mkn/nat.py en -sqllimit:200
+python3 c9/pwb.py most/bot.py
+python3 c9/pwb.py mv_bots/mvcats_run.py
+python3 c9/pwb.py mv_bots/not_empty_cats_run.py
+python3 c9/pwb.py nep/si3g.py -newpages:400 save
+python3 c9/pwb.py nep/si3g_qua.py returnlab limit:1000
+python3 c9/pwb.py neq/nldes3.py a2r sparql:Q13442814,Q13433827,Q265158,Q191067,Q19389637,Q953806 all:1000 doar
+python3 c9/pwb.py new_all/bot.py -ns:0 -newpages2:100 getred ili
+python3 c9/pwb.py new_all/bot.py -ns:0 -recentchanges:100 archive
+python3 c9/pwb.py people/new3.py limit:1000
+python3 c9/pwb.py portal/po.py -lange:enfr -newpages2:100
+python3 c9/pwb.py portal/po.py -ns:0 2030 -cat:تصنيف:مقالات_بحاجة_لشريط_بوابات
+python3 c9/pwb.py refn/r.py -ns:0,10 -recentchanges:50
+python3 c9/pwb.py refn/r_page.py
+python3 c9/pwb.py stub/b2_new.py new:تصنيف:بذرة_أعلام P106 withnoflage numbs:300 noflag
+python3 c9/pwb.py stub/p27.py country:all PP31 P31 P106
+python3 c9/pwb.py stub/p27.py ppp:qq PP31 P17
+python3 c9/pwb.py stub/p27.py ppp:تصنيف:بذرة_أعلام P27 P106
+python3 c9/pwb.py stub/stubsql.py
+python3 c9/pwb.py stub/wd.py b2 new:تصنيف:بذرة numbs:300
+python3 c9/pwb.py stub/wd.py b2 wikidata:bio cat:تصنيف:بذرة_أعلام limit:2000
+python3 c9/pwb.py wd_link/wdlink.py -ns:0,14 -newpages:150
+python3 c9/pwb.py wd_link/wdlink.py -ns:14 -usernewpages:Mr.Ibrahembot
---
+++ usage/all_paths.sh
@@ -0,0 +1,64 @@
+
+python3 core1/arwiki/params_report.py
+python3 core1/arwiki/redcat.py
+python3 core1/arwiki/rm.py
+python3 core1/arwiki/sug.py
+python3 core1/arwiki/temptitle.py
+
+python3 core1/auths/del.py
+python3 core1/catsn/tempcat.py
+python3 core1/cite/reff.py
+
+python3 core1/refn/r.py
+python3 core1/refn/r_page.py
+
+python3 core1/wd_link/wdlink.py
+
+python3 core1/new_all/bot.py
+python3 core1/portal/po.py
+
+python3 core1/ill/f.py
+python3 core1/ill/ill2.py
+python3 core1/ill/illwd2.py
+python3 core1/ill/new.py
+
+python3 asa_core/asa/ass4_gen.py
+python3 asa_core/asa/bio.py
+python3 asa_core/asa/cate.py
+python3 asa_core/asa/import.py
+python3 asa_core/asa/infobox.py
+python3 asa_core/asa/ssql.py
+
+python3 asa_core/asa1/as_by_project.py
+python3 asa_core/asa1/assu.py
+
+python3 asa_core/asapages/bot.py
+python3 asa_core/asapages/bot2.py
+
+python3 core_cat/c18/cat.py
+python3 core_cat/c18/cat2.py
+python3 core_cat/c18/catar.py
+python3 core_cat/c18/ennew_24.py
+python3 core_cat/c18/newcat.py
+python3 core_cat/c18/nnew.py
+python3 core_cat/c18/search_cats.py
+
+python3 core_cat/mkn/a.py
+python3 core_cat/mkn/nat.py
+python3 core_cat/most/bot.py
+
+python3 stub_bots/stub/b2_new.py
+python3 stub_bots/stub/p27.py
+python3 stub_bots/stub/stubsql.py
+python3 stub_bots/stub/wd.py
+
+python3 wd_core/cy/jsub.py
+
+python3 wd_core/des/fam.py
+
+python3 wd_core/nep/si3g.py
+python3 wd_core/nep/si3g_qua.py
+
+python3 wd_core/neq/nldes3.py
+
+python3 wd_core/people/new3.py
